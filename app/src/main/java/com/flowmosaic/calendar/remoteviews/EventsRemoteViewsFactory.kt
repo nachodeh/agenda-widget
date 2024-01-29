186f49e0131f775f502c71e5e367ad4ea528a638
Ignacio De Haedo <nachodeh@gmail.com>
2024-01-29 09:27:25 +0000

    Light shadows for dark texts

--- app/src/main/java/com/flowmosaic/calendar/remoteviews/EventsRemoteViewsFactory.kt
+++ app/src/main/java/com/flowmosaic/calendar/remoteviews/EventsRemoteViewsFactory.kt
@@ -2,6 +2,7 @@ package com.flowmosaic.calendar.remoteviews
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.util.TypedValue
 import android.view.Gravity
 import android.widget.RemoteViews
@@ -38,18 +39,34 @@ class EventsRemoteViewsFactory(private val context: Context) : RemoteViewsServic
         return events.size
     }
 
+    private fun isColorLight(color: Int): Boolean {
+        val red = Color.red(color) / 255.0
+        val green = Color.green(color) / 255.0
+        val blue = Color.blue(color) / 255.0
+
+        val luminance = 0.2126 * red + 0.7152 * green + 0.0722 * blue
+        return luminance > 0.5
+    }
+
     override fun getViewAt(position: Int): RemoteViews {
         val item = events[position]
 
+        val textColor = AgendaWidgetPrefs.getTextColor(context).toArgb()
+        val isColorLight = isColorLight(textColor)
+
         val layoutResId = when (item) {
-            is CalendarViewItem.Day -> R.layout.item_date
-            is CalendarViewItem.Event -> R.layout.item_event
+            is CalendarViewItem.Day -> if (isColorLight) R.layout.item_date else R.layout.item_date_dark
+            is CalendarViewItem.Event -> if (isColorLight) R.layout.item_event else R.layout.item_event_dark
         }
 
         return RemoteViews(context.packageName, layoutResId).apply {
-            val (textViewId, defaultTextSizeSp) = when (item) {
-                is CalendarViewItem.Day -> R.id.item_date_text_view to 16f
-                is CalendarViewItem.Event -> R.id.item_event_text_view to 14f
+            val defaultTextSizeSp = when (item) {
+                is CalendarViewItem.Day -> 16f
+                is CalendarViewItem.Event -> 14f
+            }
+            val textViewId = when (item) {
+                is CalendarViewItem.Day -> if (isColorLight) R.id.item_date_text_view else R.id.item_date_text_view_dark
+                is CalendarViewItem.Event -> if (isColorLight) R.id.item_event_text_view else R.id.item_event_text_view_dark
             }
             val text = when (item) {
                 is CalendarViewItem.Day -> CalendarDateUtils.getFormattedDate(context, item.date.time)
@@ -65,15 +82,13 @@ class EventsRemoteViewsFactory(private val context: Context) : RemoteViewsServic
                 AgendaWidgetPrefs.TextAlignment.CENTER -> Gravity.CENTER
                 AgendaWidgetPrefs.TextAlignment.RIGHT -> Gravity.RIGHT
             }
-
             setTextViewText(textViewId, text)
-            setTextColor(textViewId, AgendaWidgetPrefs.getTextColor(context).toArgb())
+            setTextColor(textViewId, textColor)
             setTextViewTextSize(textViewId, TypedValue.COMPLEX_UNIT_SP, defaultTextSizeSp + fontSizeAdjustment)
             setInt(textViewId, "setGravity", textAlignment)
             setOnClickFillInIntent(textViewId, getFillInIntent(item))
         }
     }
-
 
     private fun getFillInIntent(item: CalendarViewItem): Intent {
         return when (item) {
@@ -93,7 +108,7 @@ class EventsRemoteViewsFactory(private val context: Context) : RemoteViewsServic
     }
 
     override fun getViewTypeCount(): Int {
-        return 2
+        return 4
     }
 
     override fun getItemId(position: Int): Long {
