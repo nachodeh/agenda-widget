8a8fe56eb7a652e5c0452f80415beb744bba36e2
Ignacio De Haedo <nachodeh@gmail.com>
2024-02-20 11:20:14 +0000

    Polish theme

--- app/src/main/java/com/flowmosaic/calendar/ui/screens/LaunchWidgetConfigButton.kt
+++ app/src/main/java/com/flowmosaic/calendar/ui/screens/LaunchWidgetConfigButton.kt
@@ -1,12 +1,19 @@
 package com.flowmosaic.calendar.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
@@ -21,19 +28,28 @@ fun LaunchWidgetConfigButton(navHostController: NavHostController, id: Int, text
             navHostController.navigate("Widgets/$id")
         },
         colors = ButtonDefaults.buttonColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer,
-            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+            contentColor = MaterialTheme.colorScheme.onSurface
         ),
-        modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp)
-
+        modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp),
     ) {
-        Text(
-            text = text,
-            style = MaterialTheme.typography.labelLarge,
-            textAlign = TextAlign.Start,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 16.dp, horizontal = 8.dp)
-        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = text,
+                style = MaterialTheme.typography.labelLarge,
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .padding(vertical = 16.dp, horizontal = 8.dp)
+            )
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowRight,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onSurface
+            )
+        }
     }
 }
