5eaf11b4b46a748a5424015dafb3466033b6e6fb
Ignacio De Haedo <nachodeh@gmail.com>
2024-02-21 11:13:20 +0000

    Update default theme

--- app/src/main/java/com/flowmosaic/calendar/ui/theme/Theme.kt
+++ app/src/main/java/com/flowmosaic/calendar/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.flowmosaic.calendar.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -11,31 +10,23 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Green20,
+    inversePrimary = Green,
+    onPrimary = Green,
+    secondary = Green20,
+    tertiary = Green60,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Green,
+    inversePrimary = Green30,
+    onPrimary = White,
+    secondary = Green60,
+    tertiary = Green20
 )
 @Composable
 fun getPrimaryColor(): Color {
@@ -59,7 +50,6 @@ fun CalendarWidgetTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
@@ -68,7 +58,6 @@ fun CalendarWidgetTheme(
         SideEffect {
 //            val window = (view.context as Activity).window
 //            window.statusBarColor = colorScheme.primary.toArgb()
-
 //            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
