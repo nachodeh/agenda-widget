0f3534a44063a357bc6ab55c629f02553e1c302a
nachodeh <nachodeh@gmail.com>
2024-02-24 14:10:02 +0000

    Improve calendar fetching

--- app/src/main/java/com/flowmosaic/calendar/data/CalendarFetcher.kt
+++ app/src/main/java/com/flowmosaic/calendar/data/CalendarFetcher.kt
@@ -66,11 +66,11 @@ class CalendarFetcher {
         val currentTime = Calendar.getInstance().timeInMillis
         val endTime =  Calendar.getInstance().apply {
             timeInMillis = currentTime
-            add(Calendar.DAY_OF_MONTH, prefs.getNumberOfDays(widgetId) - 1)
-            set(Calendar.HOUR_OF_DAY, 23)
-            set(Calendar.MINUTE, 59)
-            set(Calendar.SECOND, 59)
-            set(Calendar.MILLISECOND, 999)
+            add(Calendar.DAY_OF_MONTH, prefs.getNumberOfDays(widgetId))
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
         }.timeInMillis
         return Pair(currentTime, endTime)
     }
