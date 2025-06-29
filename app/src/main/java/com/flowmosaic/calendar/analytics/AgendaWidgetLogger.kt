f6d8560790355977569c068ca4732493e7422807
Mike <git@mjpa.co.uk>
2025-06-29 20:31:44 +0100

    Rename preference to blob with intention of making them icons

--- app/src/main/java/com/flowmosaic/calendar/analytics/AgendaWidgetLogger.kt
+++ app/src/main/java/com/flowmosaic/calendar/analytics/AgendaWidgetLogger.kt
@@ -71,7 +71,7 @@ class AgendaWidgetLogger internal constructor(
         VERTICAL_SPACING("vertical_spacing"),
         TEXT_ALIGNMENT("text_alignment"),
         OPACITY("opacity"),
-        SHOW_CAL_COLOR("show_calendar_color")
+        SHOW_CALENDADR_BLOB("show_calendar_blob")
     }
 
     enum class WidgetStatus(val status: String) {
