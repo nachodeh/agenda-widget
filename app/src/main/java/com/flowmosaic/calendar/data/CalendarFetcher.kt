3065345efa2193ed9b27d56fa532ebfb6cc3ec92
marstaj <marstaj1@gmail.com>
2025-07-25 09:32:02 -0600

    Refactor: Clarify time adjustment logic and remove redundant variable
    This commit refactors the `CalendarFetcher` to:
    - Clarify in a KDoc comment that `adjustTimeForTimezone` only adjusts times for all-day events.
    - Remove the redundant `maxEndTime` variable within the event fetching loop, using `endTime` directly for coercing the event's end time.
    - This prevents all day events to spilling to an extra day due to unnecessary `maxEndTime` time zone conversion that happened only for all day events.

--- app/src/main/java/com/flowmosaic/calendar/data/CalendarFetcher.kt
+++ app/src/main/java/com/flowmosaic/calendar/data/CalendarFetcher.kt
@@ -143,7 +143,6 @@ class CalendarFetcher {
 
                 val startDateTime = adjustTimeForTimezone(actualStartTime, allDay)
                 val endDateTime = adjustTimeForTimezone(actualEndTime, allDay)
-                val maxEndTime = adjustTimeForTimezone(endTime, allDay)
 
                 if (endDateTime < System.currentTimeMillis()) continue // Skip past events
                 if (startDateTime >= endTime) continue // Skip events that will show as next day in current timezone.
@@ -151,7 +150,7 @@ class CalendarFetcher {
                 events.add(
                     CalendarEvent(
                         startDateTime,
-                        endDateTime.coerceAtMost(maxEndTime),
+                        endDateTime.coerceAtMost(endTime),
                         title,
                         location,
                         allDay,
@@ -166,6 +165,10 @@ class CalendarFetcher {
         return events
     }
 
+    /**
+     * Adjusts the given time by the timezone offset of the device.
+     * Important: Only ALL day events are adjusted!
+     */
     private fun adjustTimeForTimezone(time: Long, allDay: Boolean): Long {
         if (!allDay) {
             return time
